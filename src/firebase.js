fac890cfc9060c04152d6b64dbe786bbf0d1538c
EnkoMaster77 <aethernal.enterprises@gmail.com>
2025-05-05 16:25:59 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
 // src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// ตั้งค่า Firebase ของคุณ
 const firebaseConfig = {
   apiKey: "AIzaSyCqu2HlWOn2YN_Ow8mSA-_sXGE6OI507Sc",
   authDomain: "enko-app-ecdb0.firebaseapp.com",
   projectId: "enko-app-ecdb0",
-  storageBucket: "enko-app-ecdb0.firebasestorage.app",
+  storageBucket: "enko-app-ecdb0.appspot.com",
   messagingSenderId: "588332671027",
   appId: "1:588332671027:web:a02bd134953b8bbcb00346",
   measurementId: "G-HCYBP2JY2N"
 };
 
+// เริ่มต้น Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
