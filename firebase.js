64fcd66cb982c595a112910219d9439e4ef718ab
EnkoMaster77 <aethernal.enterprises@gmail.com>
2025-05-05 16:12:33 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqu2HlWOn2YN_Ow8mSA-_sXGE6OI507Sc",
+  authDomain: "enko-app-ecdb0.firebaseapp.com",
+  projectId: "enko-app-ecdb0",
+  storageBucket: "enko-app-ecdb0.firebasestorage.app",
+  messagingSenderId: "588332671027",
+  appId: "1:588332671027:web:a02bd134953b8bbcb00346",
+  measurementId: "G-HCYBP2JY2N"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
