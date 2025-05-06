73bf7a20b6d1df77e08b6a229da1206dad8342df
EnkoMaster77 <aethernal.enterprises@gmail.com>
2025-05-06 14:27:32 +0700

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
 // src/pages/Dashboard.jsx
 import React from 'react';
-import SummaryCards from '../components/SummaryCards';
-import MaterialForm from '../components/MaterialForm';
-import MaterialTable from '../components/MaterialTable';
+import SummaryCards from '../component/SummaryCards';
+import MaterialForm from '../component/MaterialForm';
+import MaterialTable from '../component/MaterialTable';
 
 export default function Dashboard({ materials, form, setForm, handleAdd, fetchMaterials }) {
   return (
